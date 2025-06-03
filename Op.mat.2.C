0f6af16282787b6c6f4b2ec59c578dfd1551c277
Programacao-japa <adriano.lera@hotmail.com>
2025-06-03 18:52:37 +0000

    Mat.Ari.edit.C

---
+++ Op.mat.2.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Atribuição Simples (=)
+    Atribuição com Soma (+=)
+    Atribuição com Subtração (-=)
+    Atribuição com Divisão (/=)
+    */
+
+    int numero1, numero2, resultado;
+
+    resultado = 10;
+    printf("Resultado: %d\n", resultado);
+
+    //resultado = resultado + 20;
+    resultado += 20;
+    printf("Resultado: %d\n", resultado);
+
+    // resultado, = resultado - numero1
+    resultado -= numero1;
+
+    resultado *= 5;
+    printf("Resultado: %d\n", resultado);
+
+    resultado /= 2;
+    printf("resultado: %d\n", resultado);
+
+
+
+}
